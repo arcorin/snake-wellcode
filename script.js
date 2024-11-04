d127c7101c73b60954eea7349c6795f2e33a90c5
arcorin <48508234+arcorin@users.noreply.github.com>
2024-11-04 16:00:52 +0200

    add food generator, increase snake length

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ startBtn.addEventListener("click", startGame);
 const pauseBtn = document.getElementById("pause-btn");
 pauseBtn.addEventListener("click", pauseGame);
 
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", resetGame);
+
 // functionality for the stop button
 const stopBtn = document.getElementById("stop-btn");
 stopBtn.addEventListener("click", stopGame);
@@ -17,6 +20,15 @@ const BLOCK_SIZE = 25;
 // else display explanatory message
 const canvas = document.getElementById("gameBoard");
 
+// the snake is initialized in the middle of the board when the game starts
+const START_SNAKE = [
+    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS), y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS)},
+    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 1, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) },
+    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 2, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) },
+    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 3, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) },
+    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 4, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) }
+];
+
 let ctx;
 if (canvas.getContext) {
     ctx = canvas.getContext("2d"); 
@@ -27,16 +39,7 @@ if (canvas.getContext) {
 
 let gameOn = false;
 
-let intervalID, newHeadPosition, stopConditions;
-
-// the snake is initialized in the middle of the board when the game starts
-let snake = [
-    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS), y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS)},
-    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 1, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) },
-    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 2, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) },
-    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 3, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) },
-    { x: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) - 4, y: Math.floor((TOTAL_BLOCKS / 2) % TOTAL_BLOCKS) }
-];
+let snake, intervalID, newHeadPosition, tail, stopConditions, food;
 
 // when the game starts - the snake moves horizontaly, to the right
 let dx = 1;
@@ -61,28 +64,53 @@ function keyPress(e) {
     }
 }
 
+
+function randomInt(max) {
+    return Math.floor(Math.random() * max); 
+}
+
+function generateFood() {
+    food = { x: randomInt(TOTAL_BLOCKS), y: randomInt(TOTAL_BLOCKS)};
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(food.x * BLOCK_SIZE, food.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+}
+
 // stop the game when:
 // - the stop button is clicked or 
 // - the snake hit the wall or itself
 function stopGame() {
-    // hide the stop button and display the start button
+    document.getElementById("message").innerText = "Game Over";
+    // hide the stop and reset buttons, and display the start button
     stopBtn.classList.add("hidden");
     pauseBtn.classList.add("hidden");
     startBtn.classList.remove("hidden");
 
-    gameOn = false;
+    gameOn = true;
 
-    // stop the main function - playGame()
+    // stop from execution the main function - playGame()
     clearInterval(intervalID);
 
-    // remove the filling of the game board
+    // remove the filling color of the game board
     ctx.clearRect(0, 0, TOTAL_BLOCKS * BLOCK_SIZE, TOTAL_BLOCKS * BLOCK_SIZE);
 
-
+    // remove the filling color of the snake
     snake.forEach( (coords) => {
-        console.log("clear")
+        console.log("clear");
         ctx.clearRect(coords.x * BLOCK_SIZE, coords.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
     });
+
+    // remove the filling color of the tail
+    ctx.clearRect(tail.x * BLOCK_SIZE, tail.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+    // remove the filling color of the food
+    ctx.clearRect(food.x * BLOCK_SIZE, food.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    
+    snake = START_SNAKE;
+}
+
+
+function resetGame() {
 
 }
 
@@ -93,7 +121,7 @@ function pauseGame() {
     pauseBtn.classList.add("hidden");
     startBtn.classList.remove("hidden");
 
-    gameOn = false;
+    
 
     // stop the main function - playGame()
     clearInterval(intervalID);
@@ -106,6 +134,10 @@ function startGame() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, TOTAL_BLOCKS * BLOCK_SIZE, TOTAL_BLOCKS * BLOCK_SIZE);
     
+    snake = START_SNAKE;
+
+    generateFood();
+
     // hide the start button and display the stop button
     startBtn.classList.add("hidden");
     pauseBtn.classList.remove("hidden");
@@ -129,9 +161,10 @@ function playGame() {
     
         // display the snake by filling each of its element
         ctx.fillStyle = "lightgreen";
+
+        // display each element of the snake
         snake.forEach( (coords, index) => {
 
-            // display each element of the snake
             console.log(`x: ${coords.x} y: ${coords.y}`)
 
             // define the stopping (game over) conditions:
@@ -147,20 +180,28 @@ function playGame() {
                 ctx.fillRect(coords.x * BLOCK_SIZE, coords.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
             };
         });
-        
+    
         // move the snake by adding a new first element
         snake.unshift(newHeadPosition);
 
-        // delete the last element of the snake 
-        let tail = snake.pop();
-        console.log(`tail x: ${tail.x} y: ${tail.y}`);
+        if (snake[0].x == food.x && snake[0].y == food.y) {
+            
+            ctx.fillStyle = "lightgreen"
+            ctx.fillRect(food.x * BLOCK_SIZE, food.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+            generateFood();
 
-        // fill the last element of the snake with the color of the game board
-        ctx.fillStyle = "black";
-        ctx.fillRect(tail.x * BLOCK_SIZE, tail.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    
-        
-    } 
+        } else {
+            
+            // delete the last element of the snake 
+            tail = snake.pop();
+            console.log(`tail x: ${tail.x} y: ${tail.y}`);
+
+            // fill the last element of the snake with the color of the game board
+            ctx.fillStyle = "black";
+            ctx.fillRect(tail.x * BLOCK_SIZE, tail.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+        }
+    }
 }
 
 
